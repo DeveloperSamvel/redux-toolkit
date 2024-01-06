d98a19ad919f4c75eda2bc5a69c94c144fdec61b
Samvel Vardanyan <129245937+DeveloperSamvel@users.noreply.github.com>
2024-01-06 18:09:23 +0400

    add logo in sidebar and fix style

--- src/layouts/Admin/Sidebar/SidebarMenu.js
+++ src/layouts/Admin/Sidebar/SidebarMenu.js
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import logo from '../../../static/images/logo.png';
 import "./SidebarMenu.css";
+
 
 function SidebarMenu({ handleLogOutUser, open }) {
   return (
     <div className={`sidebar-menu ${open ? "open" : ""}`}>
       <div className="sidebar-menu-header">
-        <h2 className="sidebar-menu-header-h2">Drawer Menu</h2>
+        <img className="sidebar-menu-header-logo" src={logo} alt="logo" />
+        <h2 className="sidebar-menu-header-h2">Admin Menu</h2>
       </div>
       <div className="sidebar-menu-body">
         <Link to="/">Home</Link>
